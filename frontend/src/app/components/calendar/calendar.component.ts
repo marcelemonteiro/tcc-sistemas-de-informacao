f317ec690ac793521f88d711c6c2042702f525a6
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-14 17:36:48 -0300

    Add field teacher in school calendar

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -10,12 +10,12 @@ import { SectionComponent } from "../section/section.component";
 })
 export class CalendarComponent {
   schoolCalendar = [
-    { id: 0, schedule: '7:30 - 8:20', class: 'Matemática' },
-    { id: 1, schedule: '8:20 - 9:10', class: 'Português' },
+    { id: 0, schedule: '7:30 - 8:20', class: 'Matemática', teacher: 'Prof. Carlos' },
+    { id: 1, schedule: '8:20 - 9:10', class: 'Português', teacher: 'Prof.ª Ana' },
     { id: 2, schedule: '9:10 - 9:30', class: 'Intervalo' },
-    { id: 3, schedule: '9:30 - 10:20', class: 'Ciências' },
-    { id: 4, schedule: '10:20 - 11:10', class: 'História' },
-    { id: 5, schedule: '11:10 - 12:00', class: 'Educação Física' },
+    { id: 3, schedule: '9:30 - 10:20', class: 'Ciências', teacher: 'Prof.ª Julia' },
+    { id: 4, schedule: '10:20 - 11:10', class: 'História', teacher: 'Prof. Pedro' },
+    { id: 5, schedule: '11:10 - 12:00', class: 'Educação Física', teacher: 'Prof. Roberto' },
   ];
 
 
