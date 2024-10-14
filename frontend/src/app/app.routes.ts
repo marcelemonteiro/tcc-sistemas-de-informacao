e647d22c759b837a4d1f018ea133f0e6c5558094
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-14 17:44:02 -0300

    Add RouterLink to Navbar component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { loginGuard } from './guards/login.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { authGuard } from './guards/auth.guard';
-import { loginGuard } from './guards/login.guard';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent, canActivate: [loginGuard] },
+  { path: 'horarios', component: CalendarComponent, canActivate: [authGuard] },
   { path: '**', component: PageNotFoundComponent },
 ];
