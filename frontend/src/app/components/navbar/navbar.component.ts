fe633b28703f4f647af158ba786e54d1e2e98686
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-15 17:01:31 -0300

    Add Icons and Logout button to Navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
