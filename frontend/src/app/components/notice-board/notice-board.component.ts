486a4a81aa0522fe5e338fe4145e577d7d88e557
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-15 19:42:40 -0300

    Create notice component

--- frontend/src/app/components/notice-board/notice-board.component.ts
+++ frontend/src/app/components/notice-board/notice-board.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SectionComponent } from "../section/section.component";
+import { NoticeComponent } from "./notice/notice.component";
 
 @Component({
   selector: 'app-notice-board',
   standalone: true,
-  imports: [SectionComponent],
+  imports: [SectionComponent, NoticeComponent],
   templateUrl: './notice-board.component.html',
   styleUrl: './notice-board.component.css'
 })
---
+++ frontend/src/app/components/notice-board/notice/notice.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-notice',
+  standalone: true,
+  imports: [RouterLink, MatIconModule],
+  templateUrl: './notice.component.html',
+  styleUrl: './notice.component.css'
+})
+export class NoticeComponent {
+  @Input() title!: string;
+  @Input() date!: string;
+  @Input() author!: string;
+  @Input() isNew!: boolean;
+
+}
