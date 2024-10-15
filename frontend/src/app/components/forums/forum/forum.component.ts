d72069a5ef5f2d4a859946b39fb03e4ee4abe14f
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-15 20:05:45 -0300

    Create forum component

---
+++ frontend/src/app/components/forums/forum/forum.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-forum',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './forum.component.html',
+  styleUrl: './forum.component.css'
+})
+export class ForumComponent {
+  @Input() title!: string;
+  @Input() lastUpdate!: string;
+  @Input() postsNumber!: number;
+  @Input() commentsNumber!: number;
+  @Input() newUpdatesNumber!: number;
+}
--- frontend/src/app/components/forums/forums.component.ts
+++ frontend/src/app/components/forums/forums.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SectionComponent } from "../section/section.component";
+import { ForumComponent } from "./forum/forum.component";
 
 @Component({
   selector: 'app-forums',
   standalone: true,
-  imports: [SectionComponent],
+  imports: [SectionComponent, ForumComponent],
   templateUrl: './forums.component.html',
   styleUrl: './forums.component.css'
 })
