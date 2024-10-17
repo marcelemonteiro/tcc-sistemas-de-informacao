d8037a4327541b9fe54cc18d0286ea01b43c5de4
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-10-17 10:59:59 -0300

    Implement page for individual notice

--- frontend/src/app/components/notice-board/notice-board.component.ts
+++ frontend/src/app/components/notice-board/notice-board.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SectionComponent } from "../section/section.component";
 import { NoticeComponent } from "./notice/notice.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notice-board',
   standalone: true,
-  imports: [SectionComponent, NoticeComponent],
+  imports: [SectionComponent, NoticeComponent, RouterLink],
   templateUrl: './notice-board.component.html',
   styleUrl: './notice-board.component.css'
 })
 export class NoticeBoardComponent {
-
+  @Input() allNoticesBtn = true;
 }
--- frontend/src/app/pages/notice-page/notice-page.component.ts
+++ frontend/src/app/pages/notice-page/notice-page.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { NoticeBoardComponent } from "../../components/notice-board/notice-board.component";
 import { DefaultLayoutComponent } from "../../components/default-layout/default-layout.component";
 import { ActivatedRoute } from '@angular/router';
+import { NoticeComponent } from '../../components/notice-board/notice/notice.component';
+import { SectionComponent } from '../../components/section/section.component';
 
 @Component({
   selector: 'app-notice-page',
   standalone: true,
-  imports: [NoticeBoardComponent, DefaultLayoutComponent],
+  imports: [NoticeBoardComponent, DefaultLayoutComponent, NoticeComponent, SectionComponent],
   templateUrl: './notice-page.component.html',
   styleUrl: './notice-page.component.css'
 })
 export class NoticePageComponent {
   id: number;
+  title = "Entrega das avaliações de matemática";
+  date = "Seg. 24 de setembro";
+  author = "Professor Billy";
 
   constructor(private route: ActivatedRoute) {
     this.id = Number(this.route.snapshot.paramMap.get("id"));
