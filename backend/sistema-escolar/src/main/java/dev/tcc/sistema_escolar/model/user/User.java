5ea286bfa7e19c2e4a73fa121f8e6c2bfb35cd7e
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-04 15:12:03 -0300

    Create User entity

---
+++ backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/model/user/User.java
@@ -0,0 +1,53 @@
+package dev.tcc.sistema_escolar.model.user;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "users")
+@Entity(name = "users")
+public class User implements UserDetails {
+    private String id;
+    private String login;
+    private String password;
+    private UserRole role;
+
+    public User() {
+    }
+
+    public User(String id, String login, String password, UserRole role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+}
