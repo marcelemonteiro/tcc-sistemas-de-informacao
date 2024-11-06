b3718c6c7032be2e0d50bb62596b45aa3c9c057b
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-06 16:53:30 -0300

    Create custom user details service

---
+++ backend2/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package dev.tcc.sistema_escolar.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import dev.tcc.sistema_escolar.domain.user.User;
+import dev.tcc.sistema_escolar.repositories.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+}
