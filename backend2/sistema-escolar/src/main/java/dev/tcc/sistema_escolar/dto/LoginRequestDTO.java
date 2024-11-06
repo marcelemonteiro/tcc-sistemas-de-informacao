87085a912188e91532e93f06acefd3eed3ec9726
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-06 17:01:52 -0300

    Create Auth and User controllers

---
+++ backend2/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package dev.tcc.sistema_escolar.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ backend2/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package dev.tcc.sistema_escolar.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {
+}
