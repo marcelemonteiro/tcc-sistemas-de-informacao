b3b159c34e72b1582db839816832fa266bdc8758
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-06 16:50:08 -0300

    Create Security filter

--- backend2/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/repositories/UserRepository.java
+++ backend2/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package dev.tcc.sistema_escolar.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.tcc.sistema_escolar.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
