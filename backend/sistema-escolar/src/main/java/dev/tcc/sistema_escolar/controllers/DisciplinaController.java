6c979826556c47116269e64fd8ff77c2dc6b4883
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-08 22:32:27 -0300

    Fix RequestBody import

--- backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/controllers/DisciplinaController.java
+++ backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/controllers/DisciplinaController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.tcc.sistema_escolar.domain.disciplina.Disciplina;
 import dev.tcc.sistema_escolar.dto.DisciplinaDTO;
 import dev.tcc.sistema_escolar.services.DisciplinaService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/disciplina")
