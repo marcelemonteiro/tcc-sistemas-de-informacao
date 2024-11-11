267b61d20f6a53578d29fc2f315ec2a29725eb74
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-11 11:54:41 -0300

    Create Nota controller

---
+++ backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/controllers/NotaController.java
@@ -0,0 +1,55 @@
+package dev.tcc.sistema_escolar.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.tcc.sistema_escolar.domain.nota.Nota;
+import dev.tcc.sistema_escolar.dto.NotaDTO;
+import dev.tcc.sistema_escolar.services.NotaService;
+
+@RestController
+@RequestMapping("/nota")
+public class NotaController {
+    @Autowired
+    private NotaService notaService;
+
+    @PostMapping
+    public ResponseEntity<Nota> create(@RequestBody NotaDTO nota) {
+        var novaNota = this.notaService.createNota(nota);
+        return ResponseEntity.ok(novaNota);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Nota> update(@PathVariable("id") String id, @RequestBody NotaDTO notaAtualizada) {
+        var nota = this.notaService.updateNote(id, notaAtualizada);
+        return ResponseEntity.ok(nota);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Nota> get(@PathVariable("id") String id) {
+        var nota = this.notaService.getNota(id);
+        return ResponseEntity.ok(nota);
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<Nota>> listAll() {
+        var notas = this.notaService.listNotas();
+        return ResponseEntity.ok(notas);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Nota> listAll(@PathVariable("id") String id) {
+        this.notaService.deleteNota(id);
+        return ResponseEntity.ok().build();
+    }
+}
