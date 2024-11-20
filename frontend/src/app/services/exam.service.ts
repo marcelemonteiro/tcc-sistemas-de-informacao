0616ff442c327c02e454da672f36bf9e67725086
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-11-20 17:02:14 -0300

    Create exam service

---
+++ frontend/src/app/services/exam.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Exam } from '../components/exam/exam.model';
+import { ExamResult } from '../interfaces/ExamResult.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExamService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  getExamListByClass(classId: string) {
+    return this.http.get<Exam[]>(
+      `http://localhost:8080/avaliacao/todos/turma/${classId}`,
+      {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + this.authService.getToken(),
+        }),
+      }
+    );
+  }
+
+  getExamResultListByStudent(studentId: string) {
+    return this.http.get<ExamResult[]>(
+      `http://localhost:8080/nota/todos/aluno/${studentId}`,
+      {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + this.authService.getToken(),
+        }),
+      }
+    );
+  }
+}
