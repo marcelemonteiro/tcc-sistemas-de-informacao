e3fca5934661d43e645d2cb49740c1dd377ab6c9
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-12-04 17:59:38 -0300

    Implement update user password

--- backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/controllers/UserController.java
+++ backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/controllers/UserController.java
@@ -1,16 +1,37 @@
 package dev.tcc.sistema_escolar.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.tcc.sistema_escolar.domain.user.User;
+import dev.tcc.sistema_escolar.dto.UpdateUserPasswordDTO;
+import dev.tcc.sistema_escolar.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping
     public ResponseEntity<String> getUser() {
         return ResponseEntity.ok("sucesso!");
+    }
+
+    @PutMapping()
+    public User updateUserPassword(@RequestBody UpdateUserPasswordDTO userPassword) {
+        return this.repository.findByEmail(userPassword.userEmail()).map(user -> {
+            user.setPassword(passwordEncoder.encode(userPassword.password()));
+            user.setEmail(user.getEmail());
+            user.setRole(user.getRole());
+            return this.repository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
---
+++ backend/sistema-escolar/src/main/java/dev/tcc/sistema_escolar/dto/UpdateUserPasswordDTO.java
@@ -0,0 +1,5 @@
+package dev.tcc.sistema_escolar.dto;
+
+public record UpdateUserPasswordDTO(String userEmail, String password) {
+
+}
