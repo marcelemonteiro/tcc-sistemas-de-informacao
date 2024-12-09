42962eef575c153dfe9b1a215b3d5d503cda9749
Marcele Monteiro <marcelepmonteiro@gmail.com>
2024-12-09 20:23:44 -0300

    admin dashboard

--- frontend/src/app/pages/home-admin/home-admin.component.ts
+++ frontend/src/app/pages/home-admin/home-admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
 import { SectionComponent } from '../../components/section/section.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-admin',
   standalone: true,
-  imports: [DefaultLayoutComponent, SectionComponent],
+  imports: [DefaultLayoutComponent, SectionComponent, RouterLink],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.css',
 })
